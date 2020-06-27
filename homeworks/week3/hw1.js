946fba9caacb4d7772eececa3f08a3747923355a
jaredWu0805 <jared810805@gmail.com>
2020-06-27 15:11:15 +0800

    HW1 done

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -5,9 +5,19 @@ const rl = readline.createInterface({
   input: process.stdin,
 });
 
+function printStars(n) {
+  for (let i = 1; i <= n; i += 1) {
+    let s = '';
+    for (let j = 1; j <= i; j += 1) {
+      s += '*';
+    }
+    console.log(s);
+  }
+}
+
 function solve(input) {
-  const tmp = input[0].split(' ');
-  console.log(Number(tmp[0]) + Number(tmp[1]));
+  const tmp = Number(input[0]);
+  printStars(tmp);
 }
 
 rl.on('line', (line) => {
