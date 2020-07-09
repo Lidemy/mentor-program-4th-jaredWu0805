975c1b28e9de36d03a4b2def5233fdcb3e6b5879
jaredWu0805 <jared810805@gmail.com>
2020-07-09 10:43:00 +0800

    hw1 reviewed

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -3,9 +3,19 @@ const https = require('https');
 https.get(
   'https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (res) => {
-    res.on('data', (chunk) => {
-      const parsed = JSON.parse(chunk);
-      parsed.forEach(book => console.log(`${book.id} ${book.name}`));
+    if (res.statusCode < 200 || res.statusCode >= 300) {
+      return console.log('Fetch failed');
+    }
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+      try {
+        const parsed = JSON.parse(rawData);
+        parsed.forEach(book => console.log(`${book.id} ${book.name}`));
+      } catch (e) {
+        console.error(e.message);
+      }
     });
+    return true;
   },
 );
