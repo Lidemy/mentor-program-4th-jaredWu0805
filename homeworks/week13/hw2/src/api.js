fd57e5cf16f1626536040f1875eb5f1212757914
jaredWu0805 <jared810805@gmail.com>
2020-09-15 12:17:03 +0800

    week13 done

---
+++ homeworks/week13/hw2/src/api.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import 'jquery';
+
+export function getApiPosts(apiUrl, data, cb) {
+	$.ajax({
+	  url: apiUrl + "api_get_posts.php",
+	  method: "POST",
+	  data: data,
+	  success: function(data) {
+	  	cb(data);
+	  }
+	});
+}
+
+
+export function addApiPost (apiUrl, data, cnt, cb) {
+	$.ajax({
+      url: apiUrl + "api_add_post.php",
+      method: "POST",
+      data: data,
+      success: function(data){
+        cb(data);
+      }
+    });
+}
---
+++ homeworks/week13/hw2/src/utils.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import $ from 'jquery';
+
+export function escapeHtml(unsafe) {
+  return unsafe.replace(/&/g, "&amp;")
+       .replace(/</g, "&lt;")
+       .replace(/>/g, "&gt;")
+       .replace(/"/g, "&quot;")
+       .replace(/'/g, "&#039;");
+}
+
+export function moreBtnShow(noMoreData) {
+  if(noMoreData) {
+    $('.more__btn').hide();
+  } else {
+    $('.more__btn').show().blur();
+  }
+}
